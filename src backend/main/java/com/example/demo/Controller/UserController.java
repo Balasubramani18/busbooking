12d10ccec72cca54a93a36304eafa5f11b45a6ea
Balasubramani18 <113656274+Balasubramani18@users.noreply.github.com>
2023-07-28 12:08:26 +0530

    Add files via upload

---
+++ src backend/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody User user) {
+        // Add validation logic here if needed
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+}
